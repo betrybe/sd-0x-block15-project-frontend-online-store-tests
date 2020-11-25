34811cae7c95f7e7f53369af372cd74da339aa0f
Mauricio Hamaji Homma <43273469+mhamaji@users.noreply.github.com>
2020-11-25 16:21:49 -0300

    Update requirement4.test.js

--- src/__tests__/requirement4.test.js
+++ src/__tests__/requirement4.test.js
@@ -5,7 +5,16 @@ import * as api from '../services/api';
 import mockedCategoriesResult from '../__mocks__/categories';
 
 jest.mock('../services/api');
-api.getCategories.mockImplementation(() => Promise.resolve(mockedCategoriesResult));
+api.getCategories.mockImplementation(() => Promise.resolve([{
+  "id": "MLB5672",
+  "name": "Acessórios para Veículos"
+}, {
+  "id": "MLB271599",
+  "name": "Agro"
+}, {
+  "id": "MLB1403",
+  "name": "Alimentos e Bebidas"
+}]));
 
 describe(`Listar as categorias de produtos disponíveis via API na página principal`, () => {
   it(`Exibe as categorias retornadas pela API na página de listagem de
