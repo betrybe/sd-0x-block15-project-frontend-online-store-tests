b8425e3957933c53222584bef2e8d60ef83ffb0b
Mauricio Hamaji Homma <43273469+mhamaji@users.noreply.github.com>
2020-11-25 23:33:19 -0300

    Update requirement4.test.js

--- src/__tests__/requirement4.test.js
+++ src/__tests__/requirement4.test.js
@@ -4,9 +4,6 @@ import CategoryList from '../components/CategoryList';
 import * as api from '../services/api';
 import mockedCategoriesResult from '../__mocks__/categories.js';
 
-jest.mock('../services/api');
-api.getCategories.mockImplementation(() => Promise.resolve(mockedCategoriesResult));
-
 describe(`Listar as categorias de produtos disponíveis via API na página principal`, () => {
   it(`Exibe as categorias retornadas pela API na página de listagem de
       produtos`, async () => {
