3417b106e189d0da2d2594452f76e4a989b68d3a
Mauricio Hamaji Homma <43273469+mhamaji@users.noreply.github.com>
2020-11-25 23:46:48 -0300

    Update requirement4.test.js

--- src/__tests__/requirement4.test.js
+++ src/__tests__/requirement4.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render, waitFor, screen } from '@testing-library/react';
-import CategoryList from '../components/CategoryList';
+import App from '../App';
 import * as api from '../services/api';
 import mockedCategoriesResult from '../__mocks__/categories.js';
 
 describe(`Listar as categorias de produtos disponíveis via API na página principal`, () => {
   it(`Exibe as categorias retornadas pela API na página de listagem de
       produtos`, async () => {
-    render(<CategoryList />);
+    render(<App />);
     expect(screen.getAllByTestId('category').length).toEqual(
       mockedCategoriesResult.length,
     );
